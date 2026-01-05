eb753781bbead8df0d133950736434e597f684d1
Riyanshi <riyanshi2674@gmail.com>
2026-01-05 08:04:58 +0530

    message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { BookList } from './books/book-list/book-list';
+import { AddBook } from './books/add-book/add-book';
+import { Login } from './auth/login/login';
+import { Signup } from './auth/signup/signup';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'books', component: BookList },
+  { path: 'add-book', component: AddBook },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup }
+];
